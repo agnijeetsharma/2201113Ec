916cf7bdd45693885eba7bde0b0e45115a8e0d64
name <123867179+agnijeetsharma@users.noreply.github.com>
2025-03-21 19:40:15 +0530

    compleetd first code

--- calculator/app.js
+++ calculator/app.js
@@ -2,6 +2,6 @@ const express = require("express");
 const numberRoutes = require("./routes/number.routes.js");
 
 const app = express();
-app.use("/", numberRoutes);
+app.use("/numbers", numberRoutes);
 
 module.exports = app;
